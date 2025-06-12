14a9bd1280cd900b1a413b9dae32e161b0f54a98
yiming-liao <yimingliao.official@gmail.com>
2025-06-12 21:29:33 +0700

    chore(build): add executable permission and usage comment to build.sh
    - set build.sh as executable script
    - add header comment explaining script purpose and usage

--- build.sh
+++ build.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
+# Build script to create a tarball package (nginx.wbm.gz)
+# for deploying the nginx Webmin module.
+#
+# Usage:
+#   1. Make sure the script is executable:
+#      chmod +x build.sh
+#   2. Run the script:
+#      ./build.sh
+#
+# This script compresses the 'nginx/' directory into 'nginx.wbm.gz',
+# which can be uploaded to Webmin for installation.
 tar czf nginx.wbm.gz nginx/
